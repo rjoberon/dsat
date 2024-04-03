f6e5019caaac3fd9289fe2a1ddd955171b3dbb30
Robert Jäschke <robert.jaeschke@hu-berlin.de>
2024-04-03 08:59:11 +0200

    initial commit

---
+++ read_mp.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#
+# Reads .mp files
+#
+# Usage:
+#
+# Author: rja
+#
+# Changes:
+# 2024-04-02 (rja)
+# - initial version
+
+import argparse
+
+version = "0.0.1"
+
+
+def get_pics(fname):
+    with open(fname, "rb") as f:
+        pos = 0
+        oldpos = 0
+        while ((b := f.read(1))):
+            pos += 1
+            if b == b'\x43':                 # C
+                b = b + f.read(3)
+                if b == b'\x43\x49\x53\x33': # CIS3
+                    #print(b.decode(), f.read(7))
+                    print(pos, pos - oldpos)
+                    oldpos = pos
+                pos += 3
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='read .mp files.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('input', type=str, help='input file')
+    parser.add_argument('-v', '--version', action="version", version="%(prog)s " + version)
+
+    args = parser.parse_args()
+
+    get_pics(args.input)
